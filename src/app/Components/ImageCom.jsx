216d68512b2aa644fd7eb520c558067633d9f353
Amr maher <amrm80233@>
2025-03-14 20:30:24 +0200

    update image style

--- src/app/Components/ImageCom.jsx
+++ src/app/Components/ImageCom.jsx
@@ -1,7 +1,7 @@
 export default function BackgroundImage() {
   return (
     <div
-      className="h-dvh rounded-ss-[50px]  hidden md:block"
+      className="h-full rounded-ss-[50px]  hidden md:block"
       style={{
         backgroundImage: "url('/Assets/chris-lee-70l1tDAI6rM-unsplash.png')",
         backgroundSize: "cover",
